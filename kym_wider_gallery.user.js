fc415081fade600f99dda858bd99279de0d98767
standoda <47794473+standoda@users.noreply.github.com>
2019-03-04 17:23:15 +0100

    Create kym_wider_gallery.user.js

---
+++ kym_wider_gallery.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Wider Gallery
+// @version      0.1
+// @author       e
+// @match        https://knowyourmeme.com/*photos*
+// @match        https://knowyourmeme.com/memes/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+var width = 1400;
+GM_addStyle ( `
+    .listings #content{width: ${width}px !important}
+    .listings #maru{max-width: ${width}px !important; min-width: ${width}px !important}
+    .listings #sidebar{display: none !important}
+`);
